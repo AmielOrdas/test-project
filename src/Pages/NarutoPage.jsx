0efc0b3bc01229108ee571db52ad48e762707fca
Amiel Ordas <100174408+AmielOrdas@users.noreply.github.com>
2024-02-28 00:35:52 +0800

    Added three buttons in naruto page with different styles. Moreover, I also added a function named processString()that removes dashes - and different variants of O such as Ō or ō.

--- src/Pages/NarutoPage.jsx
+++ src/Pages/NarutoPage.jsx
@@ -7,29 +7,59 @@ import Button from "../Button";
 function NarutoPage() {
   const [data, setData] = useState([]);
   const inputRef = useRef("Enter naruto character name");
-  function handleInputRef() {
-    console.log(inputRef.current.value);
+
+  function processString() {
+    const inputName = inputRef.current.value
+      .replace(/-/, " ")
+      .replace(/[ōŌ]/, "o") // The /[ōŌ]/ means replace any ō and Ō with small letter "o". The slashes is same with " " but general purposes.
+      .toLowerCase();
+
+    const processedName = data.filter(
+      (character) =>
+        character.name.replace(/-/, " ").replace(/[ōŌ]/, "o").toLowerCase() === // The /[ōŌ]/ means replace any ō and Ō with small letter "o".
+        inputName
+    );
+
+    return processedName;
   }
-  function handleSetCard1() {}
-  function handleSetCard2() {}
-  function handleSetCard3() {}
+
+  function handleInputRef() {
+    const processedInput = inputRef.current.value
+      .replace(/-/, " ")
+      .replace(/[ōŌ]/, "o") // The /[ōŌ]/ means replace any ō and Ō with small letter "o". The slashes is same with " " but general purposes.
+      .toLowerCase();
+
+    // console.log(processedInput);
+  }
+  function handleSetCard1() {
+    const character = processString();
+    if (character.length === 0) {
+      console.log(character);
+    } else {
+      console.log(character);
+    }
+  }
+  function handleSetCard2() {
+    const character = processString();
+  }
+  function handleSetCard3() {
+    const character = processString();
+  }
 
   async function fetchNaruto() {
     try {
       const response = await fetch(
         `https://narutodb.xyz/api/character?page=1&limit=1431`
-      ); // This will return an object containing the data
+      ); // This will return an object containing the promise.
 
       if (!response.ok) {
-        throw new Error("Could not fetch resource"); // this will execute when fetching fails
+        throw new Error("Could not fetch resource"); // This will execute when fetching fails.
       }
       const data = await response.json(); // This will convert the object into JSON format which is an array of objects.
+      console.log(data);
+      const characterArrays = data.characters.flat();
 
-      setData(data);
-      console.log(data.characters[19]);
-      console.log(data.characters.flat());
-
-      // console.log(flatData);
+      setData(characterArrays);
     } catch (error) {
       console.error(error);
     }
@@ -44,7 +74,7 @@ function NarutoPage() {
       <Navigation />
 
       <main className="flex justify-center bg-red-500 h-[32.8rem]">
-        <div className="m-auto">
+        <div className="m-auto ">
           <NarutoCard />
           <div className="flex justify-center">
             <input
@@ -55,15 +85,23 @@ function NarutoPage() {
               className="align-self-center w-[24rem]"
             ></input>
           </div>
-
-          <Button
-            text1="Add to Card 1"
-            text2="Add to Card 2"
-            text3="Add to Card 3"
-            // function1={handleCountAdd}
-            // function2={handleCountSubtract}
-            // function3={handleCountReset}
-          />
+          <div className="flex justify-between">
+            <Button
+              text={"Add to card 1"}
+              Function={handleSetCard1}
+              styles={"text-amber-300 bg-orange-600 rounded mt-2 p-2 "}
+            />
+            <Button
+              text={"Add to card 2"}
+              Function={handleSetCard2}
+              styles={"text-amber-300 bg-blue-600 rounded mt-2 p-2 "}
+            />
+            <Button
+              text={"Add to card 3"}
+              Function={handleSetCard3}
+              styles={"text-amber-300 bg-pink-400 rounded mt-2 p-2 "}
+            />
+          </div>
         </div>
 
         <div className=" bg-black"></div>
